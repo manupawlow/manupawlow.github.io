5f80a370dc8bdc2b79ec91a4827380200f370e71
manupaw <manupaw2000@gmail.com>
2022-04-07 09:58:49 -0300

    parser

--- main.js
+++ main.js
@@ -1,69 +1,124 @@
-//SETTINGS
-const ABI_PARSER_URL = 'http://localhost:7071/api/GetPepe';
 
-//FUNCTIONS
+//SETTINGS
+const ABI_PARSER_URL =
+  'https://blockchain-af-api.azurewebsites.net/api/GetPepe2?'; 
+  //'http://localhost:7071/api/GetPepe';
+
+//GLOBAL VARIABLES
+let CONTRACT_ADDRESS = '';
+let CONTRACT_ABI = '';
+
+//PAGE FUNCTIONS
+function disableAllForm(disable) {
+  var inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].disabled = disable;
+  }
+  var selects = document.getElementsByTagName("select");
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].disabled = disable;
+  }
+  var textareas = document.getElementsByTagName("textarea");
+  for (var i = 0; i < textareas.length; i++) {
+    textareas[i].disabled = disable;
+  }
+  var buttons = document.getElementsByTagName("button");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = disable;
+  }
+
+  document.getElementById("connect-btn").disabled = false;
+}
+
+const connectWallet = () => {
+  disableAllForm(true);
+
+  connect();
+
+  disableAllForm(false);
+}
+
+//PARSE FUNCTIONS
 const connect = async () => {
-    
-    await window.ethereum.enable()
-            
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    
-    const signer = provider.getSigner();
+  await window.ethereum.enable()
+        
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const signer = provider.getSigner();
 }
 
 const sendToParse = async () => {
+  const abiFormsEl = document.getElementById('abi-forms');
+  const addressEl = document.getElementById('contract-address');
+  const abiEl = document.getElementById('contract-abi');
+  const parseBtn = document.getElementById('parser-btn');
+  
+  //BEFORE PARSE
+  parseBtn.disabled = true;
+  parseBtn.value = "Parsing...";
 
-    const addressEl = document.getElementById('contract-address');
-    const abiEl = document.getElementById('contract-abi');
-    const parseBtn = document.getElementById('parse-btn');
-    
-    //BEFORE PARSE
-    parseBtn.disabled = true;
+  //PARSING
+  CONTRACT_ADDRESS = addressEl.value;
+  CONTRACT_ABI = abiEl.value;
+  
+  let success = await parse(CONTRACT_ADDRESS, CONTRACT_ABI);
+  
+  //AFTER PARSE
+  if(success) {
+    abiFormsEl.style.display = 'none';
+    abiFormsEl.style.visibility = 'hidden';
 
-    //PARSING
-    const address = addressEl.value;
-    const abi = abiEl.value;
-    
-    parse(address, abi);
-    
-    //AFTER PARSE
-    addressEl.hidden = true;
-    abiEl.hidden = true;
-    parseBtn.hidden = true;
+    document.getElementById('connect-btn').style.visibility = "visible";
+
+    connectWallet();
+
+  } else {
+    parseBtn.disabled = false;
+  }
 }
 
 const parse = async (address, abi) => {
+    let success = fetch(ABI_PARSER_URL, {
+      method: 'POST',
+      headers: {
+        'api-key': 'im an api key',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ abi: abi }),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("status: " + response.status + ". Message: " + response.statusText);
+      }
 
-    fetch(ABI_PARSER_URL, {
-        method: 'GET',
-        headers: {
-          'api-key': 'im an api key',
-          'Content-Type': 'application/json',
-        },
-        body: { abi },
-      })
-      .then(response => response.json())
-      .then(data => {
+      return response.json()
+    })
+    .then(data => {
+      console.log("Response data\n", { data });
+      
+      var divElm = document.createElement('div');
+      var scriptElm = document.createElement('script');
+      var styleElm = document.createElement('style');
 
-        console.log('Success:\n', data);
-        
-        var divElm = document.createElement('div');
-        var scriptElm = document.createElement('script');
-        var styleElm = document.createElement('style');
+      divElm.className = "parser-forms";
+      scriptElm.type = "text/javascript";
 
-        divElm.className = "html-container";
-        scriptElm.type = "text/javascript";
-
-        data.forEach(component => {
-            divElm.innerHTML += component.html;
-            scriptElm.innerHTML += component.js;
-        });
-        
-        document.body.appendChild(divElm);
-        document.body.appendChild(scriptElm);
-        document.head.appendChild(styleElm);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+      data.forEach(component => {
+          divElm.innerHTML += component.html;
+          scriptElm.innerHTML += component.js;
       });
+      
+      document.body.appendChild(divElm);
+      document.body.appendChild(scriptElm);
+      document.head.appendChild(styleElm);
+
+      return true;
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Couldnt parse :(");
+      return false;
+    });
+
+    return success;
 }
